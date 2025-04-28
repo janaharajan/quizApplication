abea59a6d9f0ef4482fe01cdd213b323a00be3a3
Janaharajan <janaharajannd2000@gmail.com>
2025-04-28 09:07:41 +0530

    commented javascript file

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let outer = [
-    container1 = {
-        question: "what is the national animal of INDIA?"
-        , option: ["Tiger", "monekey", "donkey", "rhinosoruous"],
-        answer: ["Tiger"]
-    },
-    container2 = {
-        question: "what is the national bird of INDIA?"
-        , option: ["peacock", "parrot", "crow", "eagle"],
-        answer: ["peacock"]
-    },
-    container3 = {
-        question: "Who is Father of INDIA?"
-        , option: ["gandhi", "jawaharlal nehru", "modi", "manmohan sigh"],
-        answer: ["gandhi"]
-    }
-]
-function test() {
-    if (container1.option[0] === container1.answer[0]) {
-        console.log(`correct answer is ${container1.answer[0]}`);
-console.log(outer)
-    }
-    else {
-        console.log("wrong answer")
+// let outer = [
+//     container1 = {
+//         question: "what is the national animal of INDIA?"
+//         , option: ["Tiger", "monekey", "donkey", "rhinosoruous"],
+//         answer: ["Tiger"]
+//     },
+//     container2 = {
+//         question: "what is the national bird of INDIA?"
+//         , option: ["peacock", "parrot", "crow", "eagle"],
+//         answer: ["peacock"]
+//     },
+//     container3 = {
+//         question: "Who is Father of INDIA?"
+//         , option: ["gandhi", "jawaharlal nehru", "modi", "manmohan sigh"],
+//         answer: ["gandhi"]
+//     }
+// ]
+// function test() {
+//     if (container1.option[0] === container1.answer[0]) {
+//         console.log(`correct answer is ${container1.answer[0]}`);
+// console.log(outer)
+//     }
+//     else {
+//         console.log("wrong answer")
 
-    }
-}
-test()
+//     }
+// }
+// test()
